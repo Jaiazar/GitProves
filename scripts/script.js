33448da35de8bd8fda82a10977e42d932aa8a795
Jaime <jaime.aznar.argelich@gmail.com>
2021-02-27 23:28:40 +0100

    Comienzo del proyecto v2

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector('button:first-of-type');
 const bttn = document.querySelector('button:last-of-type');
 const img = document.getElementById('image');
+const number = 20;
 
 btn.addEventListener('click', () =>{
     alert("You're sooo ugly");
@@ -9,4 +10,3 @@ btn.addEventListener('click', () =>{
 bttn.addEventListener('click', () =>{
     img.innerHTML = '<img src="https://cadenaser00.epimg.net/ser/imagenes/2020/06/01/radio_murcia/1591015955_870352_1591016675_noticia_normal_recorte1.jpg">';
 })
-
