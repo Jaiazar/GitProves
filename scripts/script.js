61a4067809abbe886623054ddbe6199aa7200c55
Jaime <jaime.aznar.argelich@gmail.com>
2021-02-27 23:47:07 +0100

    Primera modificación de la rama boss

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,13 @@
 const btn = document.querySelector('button:first-of-type');
 const bttn = document.querySelector('button:last-of-type');
 const img = document.getElementById('image');
-const number = 20;
+const number = document.getElementsByClassName('number');
 
 btn.addEventListener('click', () =>{
-    alert("You're sooo ugly");
+    alert("POP");
 })
 
 bttn.addEventListener('click', () =>{
+    number.innerHTML = '20';
     img.innerHTML = '<img src="https://cadenaser00.epimg.net/ser/imagenes/2020/06/01/radio_murcia/1591015955_870352_1591016675_noticia_normal_recorte1.jpg">';
 })
