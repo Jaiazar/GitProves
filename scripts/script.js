7e70329708ea76e14906160452e7ce0708befe0a
Jaime <jaime.aznar.argelich@gmail.com>
2021-03-01 19:37:54 +0100

    Segundo dia - v1

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,11 @@
 const btn = document.querySelector('button:first-of-type');
 const bttn = document.querySelector('button:last-of-type');
 const img = document.getElementById('image');
-const number = document.getElementsByClassName('number');
 
 btn.addEventListener('click', () =>{
     alert("POP");
 })
 
 bttn.addEventListener('click', () =>{
-    number.innerHTML = '20';
     img.innerHTML = '<img src="https://cadenaser00.epimg.net/ser/imagenes/2020/06/01/radio_murcia/1591015955_870352_1591016675_noticia_normal_recorte1.jpg">';
 })
